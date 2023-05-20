84106722f0bf06e5654d754dff4f40256b6eb4d0
kyoshinoda <kyoshisew@gmail.com>
2023-05-20 11:33:18 -0400

    added property binding and event binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   templateUrl: './courses.component.html',
 })
 export class CoursesComponent {
+  fontColor = 'pink';
+  isClicked = false;
   courses1 = 'BCS230';
   courses2 = 'BCS370';
   courses3 = 'CSC211';
   courses4 = 'MTH230';
   courses5 = 'EGL250W';
+
+  sayMessage = () => {
+    alert('test click');
+  };
 }
