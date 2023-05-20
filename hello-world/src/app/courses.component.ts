ee001b71759e2e52396f21eedb7b8df5ebb5b85d
kyoshinoda <kyoshisew@gmail.com>
2023-05-20 11:22:54 -0400

    created new component and added props

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'courses',
+  templateUrl: './courses.component.html',
+})
+export class CoursesComponent {
+  courses1 = 'BCS230';
+  courses2 = 'BCS370';
+  courses3 = 'CSC211';
+  courses4 = 'MTH230';
+  courses5 = 'EGL250W';
+}
