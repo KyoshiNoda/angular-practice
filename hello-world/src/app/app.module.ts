61ba06463533310154380d27456e342ae89e6937
kyoshinoda <kyoshisew@gmail.com>
2023-05-21 20:12:40 -0400

    added folder struct

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [AppComponent, CoursesComponent],
