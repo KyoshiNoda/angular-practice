ed039c8b7ad0ba95f3c0cbd0201c3c83c27fac07
kyoshinoda <kyoshisew@gmail.com>
2023-05-21 20:24:21 -0400

    added signedIn component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './components/courses/courses.component';
+import { SignedInComponent } from './components/signedIn/signedIn.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent, CoursesComponent, SignedInComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ hello-world/src/app/components/signedIn/signedIn.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'signedIn',
+  templateUrl: './signedIn.component.html',
+  styleUrls: ['./signedIn.component.css'],
+})
+export class SignedInComponent {}
