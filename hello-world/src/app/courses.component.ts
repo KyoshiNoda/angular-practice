e276f90a4721bac52f0adc19607b166de327659b
kyoshinoda <kyoshisew@gmail.com>
2023-05-21 20:07:02 -0400

    added style pages

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent {
   fontColor = 'pink';
